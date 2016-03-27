9a648f71565b0a8311e0b84140d8cdf3c3a32daf
Ricardo <bergerrc@users.noreply.github.com>
2016-03-27 20:48:29 -0300

    Change to php5

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ fi
 # Update Composer
 sudo /usr/bin/composer self-update
 
-# Add PHP7.0 Repository
+# Add php5 Repository
 LC_ALL=C.UTF-8 sudo add-apt-repository ppa:ondrej/php -y
 sudo apt-add-repository ppa:rwky/redis -y
 sudo apt-get update
@@ -22,10 +22,13 @@ sudo apt-get update
 # Update Heroku Toolbelt
 wget -O- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 
-# Install PHP7.0 & Redis
-sudo apt-get install -qq php7.0-fpm php7.0-cli php7.0-common php7.0-json php7.0-opcache php7.0-mysql php7.0-phpdbg \
-php7.0-mbstring php7.0-gd php7.0-imap php7.0-ldap php7.0-pgsql php7.0-pspell php7.0-recode php7.0-tidy php7.0-dev \
-php7.0-intl php7.0-gd php7.0-curl php7.0-zip php7.0-xml redis-server
+# Install php5 & Redis
+sudo apt-get install -qq php5-fpm php5-cli php5-common php5-json php5-mysql \
+php5-gd php5-imap php5-ldap php5-pgsql php5-pspell php5-recode php5-tidy php5-dev \
+php5-intl php5-gd php5-curl redis-server
+
+#sudo apt-get install -qq php5-phpdbg php5-opcache php5-mbstring php5-zip php5-xml
+
 sudo apt-get purge -qq apache2 mysql-server mysql-client
 
 # Stop all the services
@@ -48,36 +51,37 @@ sudo ln -s /etc/nginx/sites-available/c9 /etc/nginx/sites-enabled/c9
 
 
 # PHP:
-sudo sed -i 's/user = www-data/user = ubuntu/g' /etc/php/7.0/fpm/pool.d/www.conf
-sudo sed -i 's/group = www-data/group = ubuntu/g' /etc/php/7.0/fpm/pool.d/www.conf
-sudo sed -i 's/pm = dynamic/pm = ondemand/g' /etc/php/7.0/fpm/pool.d/www.conf # Reduce number of processes..
+sudo sed -i 's/user = www-data/user = ubuntu/g' /etc/php5/fpm/pool.d/www.conf
+sudo sed -i 's/group = www-data/group = ubuntu/g' /etc/php5/fpm/pool.d/www.conf
+sudo sed -i 's/pm = dynamic/pm = ondemand/g' /etc/php5/fpm/pool.d/www.conf # Reduce number of processes..
 
 # Install Redis Driver
 cd ~
 git clone https://github.com/phpredis/phpredis.git
 cd phpredis
-git checkout php7
+git checkout php5
 phpize
 ./configure
 make && sudo make install
 cd ..
 rm -rf phpredis
-sudo sh -c 'echo "extension=redis.so" > /etc/php/7.0/mods-available/redis.ini'
-sudo ln -sf /etc/php/7.0/mods-available/redis.ini /etc/php/7.0/fpm/conf.d/20-redis.ini
-sudo ln -sf /etc/php/7.0/mods-available/redis.ini /etc/php/7.0/cli/conf.d/20-redis.ini
+sudo sh -c 'echo "extension=redis.so" > /etc/php5/mods-available/redis.ini'
+sudo ln -sf /etc/php5/mods-available/redis.ini /etc/php5/fpm/conf.d/20-redis.ini
+sudo mkdir /etc/php5/conf.d/
+sudo ln -sf /etc/php5/mods-available/redis.ini /etc/php5/conf.d/20-redis.ini
 
 # Install helper
-sudo wget https://raw.githubusercontent.com/GabrielGil/c9-lemp/master/lemp --output-document=/usr/bin/lemp
+sudo wget https://raw.githubusercontent.com/bergerrc/c9-lemp/master/lemp --output-document=/usr/bin/lemp
 sudo chmod 755 /usr/bin/lemp
 
 # Start the party!
 sudo service nginx start
 sudo service nginx reload
-sudo service php7.0-fpm start
+sudo service php5-fpm start
 
 # Are we ready?
 echo Check all services are up.
 sleep 5
 sudo service nginx status
-sudo service php7.0-fpm status
+sudo service php5-fpm status
 lemp restart
